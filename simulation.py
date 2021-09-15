b350aba57ef081cabcf2be82eadaaec769b61088
Derek Wu <54968178+dyswu@users.noreply.github.com>
2021-09-14 17:56:04 -0700

    Add files via upload
    Update simulation to fix EOF error

--- simulation.py
+++ simulation.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 25 02:14:10 2021
+import random
+from replisome import *
 
-@author: derew
-"""
+random.seed()
 
 class simulation:  
-    #length of track, number of replisomes, min speed, max speed, percentage time 1, percentage time 2
-    def __init__(self, length, n, mins, maxs, tone, ttwo):
-        self.time = 100
+    #length of track, number of replisomes, min speed, max speed, percentage time 1, percentage time 2, 
+    #speed modifier for 1st label, speed modified for 2nd label
+    
+    def __init__(self, length, n, mins, maxs, tone, ttwo, speed1, speed2):
+        self.time = 1000
         self.current = 0
         self.type = 0
 
@@ -20,6 +20,7 @@ class simulation:
         self.length = length
         self.track = [-1] * length
         self.replisomes = []
+        self.stopped = []
         #Create positive and negative direction for each origin event
         for i in range(n):
             startloc = random.randint(1, length-1)
@@ -27,47 +28,76 @@ class simulation:
             
             self.replisomes.append(replisome(startloc, 
                                              starttime, 
-                                             random.randint(mins, maxs)))
+                                             round(random.randint(mins, maxs)*speed1)))
             self.replisomes.append(replisome(startloc-1, 
                                              starttime, 
-                                             -random.randint(mins, maxs)))
+                                             -round(random.randint(mins, maxs)*speed2)))
         #for i in range(len(self.replisomes)):
             #print(self.replisomes[i].loc, self.replisomes[i].time, self.replisomes[i].speed)
     def step(self):
+        #set up current label
         if (self.current == self.before):
             self.type = 1
         if (self.current == self.switch):
             self.type = 2
         if (self.current == self.after):
             self.type = 0
-            
+        
+        #Run through all replisomes in the set
         for replisome in self.replisomes:
             location = replisome.loc
             speed = replisome.speed
+            age = self.current - replisome.time
+            
+            #Check if current replisome stops at current time
+            if (age*random.random() > self.time/len(self.replisomes)):
+                #print("stop old age", replisome.speed)
+                replisome.update(location, False)
+                self.stopped.append(replisome)
+                self.replisomes.remove(replisome)
+                break
             if (replisome.check(self.current)):
                 if (location >= self.length-1 or location <= 0):
                     replisome.update(location, False)
+                    self.stopped.append(replisome)
                     self.replisomes.remove(replisome)
+                    break
                 if (speed > 0):
                     for i in range(speed):
+                        if (location >= self.length-1):
+                            #print("reach end")
+                            self.stopped.append(replisome)
+                            self.replisomes.remove(replisome)
+                            break
                         if (self.track[location] == -1):
                             self.track[location] = self.type
                             location += 1
                             replisome.update(location, True)
                         else:
                             replisome.update(location, False)
-                            #self.replisomes.remove(replisome)
+                            self.stopped.append(replisome)
+                            self.replisomes.remove(replisome)
                             break
                 else:
                     for i in range(abs(speed)):
+                        if (location <= 0):
+                            #print("reach begining")
+                            replisome.update(location, False)
+                            self.stopped.append(replisome)
+                            self.replisomes.remove(replisome)
+                            break
                         if (self.track[location] == -1):
                             self.track[location] = self.type
                             location -= 1
                             replisome.update(location, True)
                         else:
                             replisome.update(location, False)
-                            #self.replisomes.remove(replisome)
+                            self.stopped.append(replisome)
+                            self.replisomes.remove(replisome)
                             break
         self.current += 1
+        
+    def gettrack(self):
+        return self.track
         
                 
