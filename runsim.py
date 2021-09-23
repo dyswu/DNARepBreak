aff7421d2e67293b061484a8ba24b145de7a8f52
Derek Wu <54968178+dyswu@users.noreply.github.com>
2021-09-23 16:13:25 -0700

    Add files via upload
    Updated runsim to convert into dataframes

--- runsim.py
+++ runsim.py
@@ -10,14 +10,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-
+from simulation import *
 from parse import *
 
 record = []
 #length of total, number of replisomes, min speed, max speed, % of the time in label 1, % of time in label 2, label 1 speed(%), label 2 speed(%)
-sim = simulation(10000, 100, 1, 10, 100, 100, 1, 1)
+sim = simulation(10000, 100, 5, 10, 100, 100, 1, 1)
 print("time = ", sim.time, 
       "current time = ",
       sim.current, 
@@ -39,3 +37,11 @@ plt.imshow(np.array(record), interpolation='nearest', aspect='auto')
 plt.colorbar()
 plt.show()
 #record.copy()
+
+terminated, origin, ongoing, converged, diverged = parse(sim.track)
+
+terminateddf = pd.DataFrame(terminated)
+origindf = pd.DataFrame(origin)
+ongoingdf = pd.DataFrame(ongoing)
+convergeddf = pd.DataFrame(converged)
+divergeddf = pd.DataFrame(diverged)
